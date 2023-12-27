ba5cf40af9ba3f4dcfffabfd265edf2896692419
zaypaihtet <zaypaihtet@gmail.com>
2023-12-27 14:37:18 +0630

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   main.py

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/ python3
+
+import os,requests,json,random
+import getpass
+import subprocess
+os.system("clear")
+username = getpass.getuser()
+os.system("git pull")
+RED = '\033[1;91m' #
+WHITE = '\033[1;97m' #
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m' #
+BLUE = '\033[1;34m' #
+ORANGE = '\033[1;35m' #
+RESET = "\033[0m"
+BOLD	 = "\033[1m"
+MAGENTA = "\033[95m"
+CYAN = "\033[96m"
+color = [RED,GREEN,BLUE,MAGENTA,CYAN,ORANGE]
+rand = random.choice(color)
+ver = open("./src/version")
+version =  ver.read()
+
+def banner():
+    os.system("figlet X Framework | lolcat")
+    print(f"""{BOLD}{rand}
+          {GREEN}Author : {CYAN}X-Coder
+          {GREEN}Version :{CYAN}{version}
+          {BLUE}Contact : {CYAN}https://t.me/x_c0d3r
+          """)
+    print(f"{BOLD}{YELLOW}This is for educational purposes only and I will not be held responsible if anything happens")
+
+
+
+def menu():
+    banner()
+    print("type help to usage")
+    while True:
+        option = input(f"{BOLD}{GREEN}{username}@X_framwwork >> {RESET}")
+        if option in ["help","HELP","Help","option"]:
+            list = f"""{BOLD}
+            [1] -- Track Ip 
+            [Type] -> Track
+            [2] -- SMS Spam
+            [Type] -> SMS
+            [3] -- Phishing 
+            [Type] -> Phishing
+            [4] -- Gmail Clone
+            [Type] -> Gmail
+            """
+            print(list)
+        elif option == "Track":
+            subprocess.run(['python3',"./src/trackip.py" ], check=True)
+        elif option == "SMS":
+            os.system("cd src && python3 sms.py")
+        elif option in ["exit","break","end"]:
+            print(f"{BOLD}Thanks For Using My Tool")
+            exit()
+        elif option in ["3","Phishing"]:
+            os.system("cd src && python3 phishing.py")
+        elif option in ["4","Gmail"]:
+            os.system("cd src && python3 gmail.py")
+        elif option in ["ls","clear"]:
+            os.system(f"{option}")
+            banner()
+        elif option in ['banner']:
+            banner()
+
+        else:
+            print(f"{RED}Wrong input...")
+
+
+
+menu()
